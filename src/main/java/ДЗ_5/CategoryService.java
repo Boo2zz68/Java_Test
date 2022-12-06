cd36dff88ebe0e0ce82e04d3f9daa350d23b4331
AButusov <boo2zz75384@mail.ru>
2022-12-06 18:44:56 +0300

    2

---
+++ src/main/java/ДЗ_5/CategoryService.java
@@ -0,0 +1,9 @@
+package ДЗ_5;
+import ДЗ_5.dto.GetCategoryResponse;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.*;
+public interface CategoryService {
+    @GET("categories/{id}")
+    Call<GetCategoryResponse> getCategory(@Path("id") int id);
+}
